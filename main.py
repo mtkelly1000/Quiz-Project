21d65126f63d92c109cb7d811806c7d16e31f934
Madison K <mtkelly1000@gmail.com>
2020-08-03 21:21:52 +0000

    added quit

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-j = input('hello! What is your name?')
-print ('nice to meet you, ' + j)
-
-Q1 = input ('What is your favorite color? \nA) blue \nB) red')
-Q2 = input ('What is your favorite dessert? \nA) cake \nB) ice cream ')
-Q3 = input ('what is your favorite animal: \nA) cat  \nB) dog')
-
 
 answercombo = {
   'AAA':'Maddy',
@@ -13,6 +6,17 @@ answercombo = {
   'ABB':'Eli'
 }
 
-code = Q1+Q2+Q3
-if code in answercombo:
+
+j = input('hello! What is your name?')
+print ('nice to meet you, ' + j)
+
+quit= 'y'
+while quit == 'y':
+  Q1 = input ('What is your favorite color? \nA) blue \nB) red')
+  Q2 = input ('What is your favorite dessert? \nA) cake \nB) ice cream ')
+  Q3 = input ('what is your favorite animal: \nA) cat  \nB) dog')
+  code = Q1+Q2+Q3
+  if code in answercombo:
     print ('you are '+ answercombo.get(code))
+
+  quit = input ('do you want to continue? (y/n)')
